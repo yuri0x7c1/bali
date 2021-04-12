f2f5c2af6e2468c6212b647ce5fba2fdacb09c36
yuri0x7c1 <yuri0x7c1@gmail.com>
2021-04-12 22:12:48 +0300

    Port vaadin4spring i18n module as bali-ui-i18n

---
+++ bali-ui-i18n/src/main/java/com/github/yuri0x7c1/bali/ui/i18n/config/I18NConfiguration.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 The original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.yuri0x7c1.bali.ui.i18n.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.github.yuri0x7c1.bali.ui.i18n.CompositeMessageSource;
+import com.github.yuri0x7c1.bali.ui.i18n.I18N;
+import com.github.yuri0x7c1.bali.ui.i18n.MessageProviderCacheCleanupExecutor;
+
+/**
+ * Configuration class used by {@literal @}EnableVaadinI18N
+ * 
+ * Spring configuration for the {@link com.github.yuri0x7c1.bali.ui.i18n.CompositeMessageSource}. Please remember to
+ * define {@link com.github.yuri0x7c1.bali.ui.i18n.MessageProvider} beans that can serve the message source with messages.
+ * 
+ * @author Gert-Jan Timmer (gjr.timmer@gmail.com)
+ * @author Petter Holmström (petter@vaadin.com)
+ * @see com.github.yuri0x7c1.bali.ui.i18n.I18N
+ * @see com.github.yuri0x7c1.bali.ui.i18n.CompositeMessageSource
+ */
+@Configuration
+public class I18NConfiguration {
+
+    @Bean
+    I18N i18n(ApplicationContext context) {
+        return new I18N(context);
+    }
+
+    @Bean
+    CompositeMessageSource messageSource(ApplicationContext context) {
+        return new CompositeMessageSource(context);
+    }
+
+    @Bean
+    MessageProviderCacheCleanupExecutor messageProviderCacheCleanupExecutor(Environment environment,
+        CompositeMessageSource messageSource) {
+        return new MessageProviderCacheCleanupExecutor(environment, messageSource);
+    }
+}
