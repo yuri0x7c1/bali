3113f92ac15e8340f77dcdbd5a3b78d0a0b0874c
yuri0x7c1 <yuri0x7c1@gmail.com>
2021-05-16 13:33:15 +0300

    Add common main view

---
+++ bali-ui/src/main/java/com/github/yuri0x7c1/bali/ui/view/CommonMainView.java
@@ -0,0 +1,102 @@
+package com.github.yuri0x7c1.bali.ui.view;
+
+import java.util.Optional;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.tabs.TabsVariant;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
+
+/**
+ * Common Main View
+ *
+ * @author yuri0x7c1
+ */
+public abstract class CommonMainView extends AppLayout {
+
+    private final Tabs menu;
+    private H1 viewTitle;
+
+    public CommonMainView() {
+        setPrimarySection(Section.DRAWER);
+        addToNavbar(true, createHeaderContent());
+        menu = createMenu();
+        addToDrawer(createDrawerContent(menu));
+    }
+
+    private Component createHeaderContent() {
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setId("header");
+        layout.getThemeList().set("dark", true);
+        layout.setWidthFull();
+        layout.setSpacing(false);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.add(new DrawerToggle());
+        viewTitle = new H1();
+        layout.add(viewTitle);
+        layout.add(new Avatar());
+        return layout;
+    }
+
+    private Component createDrawerContent(Tabs menu) {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        layout.getThemeList().set("spacing-s", true);
+        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
+        HorizontalLayout logoLayout = new HorizontalLayout();
+        logoLayout.setId("logo");
+        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        logoLayout.add(new Image("images/logo.png", "Bali Demo App logo"));
+        logoLayout.add(new H1("Bali Demo App"));
+        layout.add(logoLayout, menu);
+        return layout;
+    }
+
+    private Tabs createMenu() {
+        final Tabs tabs = new Tabs();
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
+        tabs.setId("tabs");
+        tabs.add(createMenuItems());
+        return tabs;
+    }
+
+    protected abstract Tab[] createMenuItems();
+
+    private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
+        final Tab tab = new Tab();
+        tab.add(new RouterLink(text, navigationTarget));
+        ComponentUtil.setData(tab, Class.class, navigationTarget);
+        return tab;
+    }
+
+    @Override
+    protected void afterNavigation() {
+        super.afterNavigation();
+        getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
+        viewTitle.setText(getCurrentPageTitle());
+    }
+
+    private Optional<Tab> getTabForComponent(Component component) {
+        return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
+                .findFirst().map(Tab.class::cast);
+    }
+
+    private String getCurrentPageTitle() {
+        PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
+        return title == null ? "" : title.value();
+    }
+}
