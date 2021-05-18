7b7d5839d3aa970126faeb231dd6dcd4b6048afa
yuri0x7c1 <yuri0x7c1@gmail.com>
2021-05-18 22:05:51 +0300

    Add BigDecimal to Long Converter

---
+++ bali-ui/src/main/java/com/github/yuri0x7c1/bali/ui/data/converter/BigDecimalToLongConverter.java
@@ -0,0 +1,26 @@
+package com.github.yuri0x7c1.bali.ui.data.converter;
+
+import java.math.BigDecimal;
+
+import com.vaadin.flow.data.binder.Result;
+import com.vaadin.flow.data.binder.ValueContext;
+import com.vaadin.flow.data.converter.Converter;
+
+/**
+ * BigDecimal to Long Converter
+ *
+ * @author yuri0x7c1
+ */
+public class BigDecimalToLongConverter implements Converter<BigDecimal, Long> {
+	@Override
+	public Result<Long> convertToModel(BigDecimal value, ValueContext context) {
+		if (value == null) return Result.ok(null);
+		return Result.ok(value.longValue());
+	}
+
+	@Override
+	public BigDecimal convertToPresentation(Long value, ValueContext context) {
+		if (value == null) return null;
+		return BigDecimal.valueOf(value);
+	}
+}
