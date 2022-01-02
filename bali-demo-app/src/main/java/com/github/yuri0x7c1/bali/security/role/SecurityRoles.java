c369c00cf4df710fee884eeb3e98b72c805da8c9
yuri0x7c1 <yuri0x7c1@gmail.com>
2022-01-02 11:15:20 +0200

    Add Apache license header to java source files

--- bali-demo-app/src/main/java/com/github/yuri0x7c1/bali/security/role/SecurityRoles.java
+++ bali-demo-app/src/main/java/com/github/yuri0x7c1/bali/security/role/SecurityRoles.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021-2022 The original authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.github.yuri0x7c1.bali.security.role;
 public enum SecurityRoles {
 	ROLE_BAR_CREATE, ROLE_BAR_VIEW, ROLE_BAR_EDIT, ROLE_BAR_DELETE, ROLE_FOO_CREATE, ROLE_FOO_VIEW, ROLE_FOO_EDIT, ROLE_FOO_DELETE, ROLE_PARTY_CREATE, ROLE_PARTY_VIEW, ROLE_PARTY_EDIT, ROLE_PARTY_DELETE, ROLE_PARTY_ROLE_CREATE, ROLE_PARTY_ROLE_VIEW, ROLE_PARTY_ROLE_EDIT, ROLE_PARTY_ROLE_DELETE, ROLE_ROLE_TYPE_CREATE, ROLE_ROLE_TYPE_VIEW, ROLE_ROLE_TYPE_EDIT, ROLE_ROLE_TYPE_DELETE, ROLE_SECURITY_GROUP_CREATE, ROLE_SECURITY_GROUP_VIEW, ROLE_SECURITY_GROUP_EDIT, ROLE_SECURITY_GROUP_DELETE, ROLE_SECURITY_ROLE_CREATE, ROLE_SECURITY_ROLE_VIEW, ROLE_SECURITY_ROLE_EDIT, ROLE_SECURITY_ROLE_DELETE, ROLE_SECURITY_USER_CREATE, ROLE_SECURITY_USER_VIEW, ROLE_SECURITY_USER_EDIT, ROLE_SECURITY_USER_DELETE, ROLE_ORGANIZATION_CREATE, ROLE_ORGANIZATION_VIEW, ROLE_ORGANIZATION_EDIT, ROLE_ORGANIZATION_DELETE, ROLE_PERSON_CREATE, ROLE_PERSON_VIEW, ROLE_PERSON_EDIT, ROLE_PERSON_DELETE
