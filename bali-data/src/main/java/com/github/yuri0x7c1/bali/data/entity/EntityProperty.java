de8071a587509ca9593612b4193a5a10d62dfee5
yuri0x7c1 <yuri0x7c1@gmail.com>
2022-03-12 14:22:35 +0200

    Implement builder for EntityProperty

--- bali-data/src/main/java/com/github/yuri0x7c1/bali/data/entity/EntityProperty.java
+++ bali-data/src/main/java/com/github/yuri0x7c1/bali/data/entity/EntityProperty.java
@@ -22,9 +22,7 @@ import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -34,42 +32,45 @@ import lombok.extern.slf4j.Slf4j;
  * @param <T>
  */
 @Slf4j
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class EntityProperty<T> {
 	@Getter
-	final String name;
+	private final String name;
 
 	@Getter
-	final String caption;
+	private final String caption;
 
 	@Getter
-	boolean sortable = true;
+	private final boolean sortable;
 
 	@Getter
-	Function<T, String> valueProvider;
+	private final Function<T, String> valueProvider;
 
+	private EntityProperty(Builder<T> builder) {
+		this.name = builder.name;
+		this.caption = builder.caption;
+		this.sortable = builder.sortable;
+		this.valueProvider = builder.valueProvider;
+	}
+
+	@Deprecated
 	public EntityProperty(String name, String caption) {
-		this.name = name;
-		this.caption = caption;
+		this(name, caption, true, null);
 	}
 
+	@Deprecated
 	public EntityProperty(String name, String caption, boolean sortable) {
-		this.name = name;
-		this.caption = caption;
-		this.sortable = sortable;
+		this(name, caption, sortable, null);
 	}
 
+	@Deprecated
 	public EntityProperty(String name, String caption, Function<T, String> valueProvider) {
-		this.name = name;
-		this.caption = caption;
-		this.valueProvider = valueProvider;
+		this(name, caption, true, valueProvider);
 	}
 
+	@Deprecated
 	public EntityProperty(String name, String caption, boolean sortable, Function<T, String> valueProvider) {
-		this.name = name;
-		this.caption = caption;
-		this.sortable = sortable;
-		this.valueProvider = valueProvider;
+		this(new Builder<T>().withName(name).withCaption(caption).withSortable(sortable)
+				.withValueProvider(valueProvider));
 	}
 
 	public Object getValue(T entity) {
@@ -108,5 +109,43 @@ public class EntityProperty<T> {
 
 	public String getValueAsString(T entity) {
 		return getValueAsString(entity, "");
+	}
+
+	public static <T> Builder<T> builder() {
+		return new Builder<T>();
+	}
+
+	public static final class Builder<T> {
+		private String name;
+		private String caption;
+		private boolean sortable = true;
+		private Function<T, String> valueProvider;
+
+		private Builder() {
+		}
+
+		public Builder<T> withName(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder<T> withCaption(String caption) {
+			this.caption = caption;
+			return this;
+		}
+
+		public Builder<T> withSortable(boolean sortable) {
+			this.sortable = sortable;
+			return this;
+		}
+
+		public Builder<T> withValueProvider(Function<T, String> valueProvider) {
+			this.valueProvider = valueProvider;
+			return this;
+		}
+
+		public EntityProperty<T> build() {
+			return new EntityProperty<T>(this);
+		}
 	}
 }
