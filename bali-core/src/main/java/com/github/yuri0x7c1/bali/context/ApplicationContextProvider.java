a9443f5c194ff987f2904cefeb4964ed9fafabb3
yuri0x7c1 <yuri0x7c1@gmail.com>
2022-05-01 15:41:11 +0300

    Fix static application context provider

--- bali-core/src/main/java/com/github/yuri0x7c1/bali/context/ApplicationContextProvider.java
+++ bali-core/src/main/java/com/github/yuri0x7c1/bali/context/ApplicationContextProvider.java
@@ -20,6 +20,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -37,6 +38,7 @@ import lombok.extern.slf4j.Slf4j;
  * @see org.springframework.context.ApplicationContext
  */
 @Slf4j
+@Component
 public class ApplicationContextProvider implements InitializingBean, ApplicationContextAware {
 
     private static ApplicationContext context;
