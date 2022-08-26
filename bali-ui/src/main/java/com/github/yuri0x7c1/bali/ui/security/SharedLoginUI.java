087bea09fcc3ca9c3b97c5432960e7b05015d8e3
yuri0x7c1 <yuri0x7c1@gmail.com>
2022-08-26 20:12:07 +0300

    Update SharedLoginUI

--- bali-ui/src/main/java/com/github/yuri0x7c1/bali/ui/security/SharedLoginUI.java
+++ bali-ui/src/main/java/com/github/yuri0x7c1/bali/ui/security/SharedLoginUI.java
@@ -19,13 +19,17 @@ package com.github.yuri0x7c1.bali.ui.security;
 import org.springframework.security.core.AuthenticationException;
 import org.vaadin.spring.i18n.I18N;
 import org.vaadin.spring.security.shared.VaadinSharedSecurity;
+import org.vaadin.viritin.layouts.MVerticalLayout;
 
-import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
-import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.PasswordField;
@@ -42,6 +46,7 @@ import lombok.extern.slf4j.Slf4j;
  * UI for the login screen.
  *
  * @author Petter Holmström (petter@vaadin.com)
+ * @author yuri0x7c1
  */
 @Slf4j
 @RequiredArgsConstructor
@@ -49,26 +54,27 @@ public class SharedLoginUI extends UI {
 
 	@NonNull
 	I18N i18n;
-	
+
     @NonNull
     VaadinSharedSecurity vaadinSecurity;
 
-    private TextField userName;
+    private TextField username;
 
-    private PasswordField passwordField;
+    private PasswordField password;
 
     private CheckBox rememberMe;
 
     private Button login;
 
     private Label loginFailedLabel;
-    
+
     private Label loggedOutLabel;
-    
+
     @Override
     protected void init(VaadinRequest request) {
         getPage().setTitle(i18n.get("Application.name"));
 
+        /*
         FormLayout loginForm = new FormLayout();
         loginForm.setSizeUndefined();
 
@@ -111,15 +117,91 @@ public class SharedLoginUI extends UI {
         rootLayout.setComponentAlignment(loginLayout, Alignment.MIDDLE_CENTER);
         setContent(rootLayout);
         setSizeFull();
+        */
+
+        Component loginForm = buildLoginForm(request);
+        setContent(
+        	new MVerticalLayout(
+        		loginForm
+        	)
+        	.withFullSize()
+        	.withMargin(true)
+        	.withSpacing(false)
+        	.withComponentAlignment(loginForm, Alignment.MIDDLE_CENTER)
+        );
+    }
+
+    private Component buildLoginForm(VaadinRequest request) {
+        final VerticalLayout loginLayout = new VerticalLayout();
+        loginLayout.addStyleName(ValoTheme.LAYOUT_CARD);
+        loginLayout.setSizeUndefined();
+        loginLayout.setMargin(true);
+        // Responsive.makeResponsive(loginPanel);
+        loginLayout.addStyleName("bali-login-panel");
+
+        if (request.getParameter("logout") != null) {
+            loggedOutLabel = new Label("You have been logged out!");
+            loggedOutLabel.addStyleName(ValoTheme.LABEL_SUCCESS);
+            loggedOutLabel.setWidthFull();
+            loginLayout.addComponent(loggedOutLabel);
+            loginLayout.setComponentAlignment(loggedOutLabel, Alignment.BOTTOM_CENTER);
+        }
+
+        loginLayout.addComponent(loginFailedLabel = new Label());
+        loginLayout.setComponentAlignment(loginFailedLabel, Alignment.BOTTOM_CENTER);
+        loginFailedLabel.setWidthFull();
+        loginFailedLabel.addStyleName(ValoTheme.LABEL_FAILURE);
+        loginFailedLabel.setVisible(false);
+
+
+        loginLayout.addComponent(buildLabels());
+        loginLayout.addComponent(buildFields());
+        rememberMe = new CheckBox("Remember me", true);
+        loginLayout.addComponent(rememberMe);
+        return loginLayout;
+    }
+
+    private Component buildFields() {
+        HorizontalLayout fields = new HorizontalLayout();
+        fields.addStyleName("fields");
+
+        username = new TextField("Username");
+        username.setIcon(FontAwesome.USER);
+        username.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
+
+        password = new PasswordField("Password");
+        password.setIcon(FontAwesome.LOCK);
+        password.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
+
+        login = new Button("Sign In");
+        login.addStyleName(ValoTheme.BUTTON_PRIMARY);
+        login.setClickShortcut(KeyCode.ENTER);
+        login.focus();
+
+        fields.addComponents(username, password, login);
+        fields.setComponentAlignment(login, Alignment.BOTTOM_LEFT);
+
+        login.addClickListener(event -> login());
+        return fields;
+    }
+
+    private Component buildLabels() {
+        CssLayout labels = new CssLayout();
+        Label title = new Label(i18n.get("Application.name"));
+        title.setSizeUndefined();
+        title.addStyleName(ValoTheme.LABEL_H3);
+        title.addStyleName(ValoTheme.LABEL_LIGHT);
+        labels.addComponent(title);
+        return labels;
     }
 
     private void login() {
         try {
-            vaadinSecurity.login(userName.getValue(), passwordField.getValue(), rememberMe.getValue());
+            vaadinSecurity.login(username.getValue(), password.getValue(), rememberMe.getValue());
         } catch (AuthenticationException ex) {
-            userName.focus();
-            userName.selectAll();
-            passwordField.setValue("");
+            username.focus();
+            username.selectAll();
+            password.setValue("");
             loginFailedLabel.setValue(String.format("Login failed: %s", ex.getMessage()));
             loginFailedLabel.setVisible(true);
             if (loggedOutLabel != null) {
